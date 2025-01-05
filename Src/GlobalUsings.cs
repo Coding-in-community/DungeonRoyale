f5ef20b5cb19ba277951c3237c5ff5d24f0fe124
Leonardo Cruz <leonardo.m.cruz@hotmail.com>
2025-01-05 13:20:11 -0300

    feat: foi criada classe que contem os inputs mapeados, scripts compartilhados foram adicionados ao global usings

--- Src/GlobalUsings.cs
+++ Src/GlobalUsings.cs
@@ -1,2 +1,3 @@
 
 global using Godot;
+global using DungeonRoyale.Src.Shared.Scripts;
---
+++ Src/Shared/Scripts/MappedInputs.cs
@@ -0,0 +1,9 @@
+namespace DungeonRoyale.Src.Shared.Scripts;
+
+public static class MappedInputs
+{
+    public const string Left = "left";
+    public const string Right = "right";
+    public const string Up = "up";
+    public const string Down = "down";
+}
